3e65a9d1823ce227a2d70990ef38791da6a492d3
Mahmoud Adas <mido3ds@gmail.com>
2017-10-11 22:33:10 +0200

    Added ViewPager but TabLayout doesnt work

--- app/src/main/java/com/example/android/miwok/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/Activities/MainActivity.java
@@ -1,46 +1,24 @@
 package com.example.android.miwok.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.example.android.miwok.R;
+import com.example.android.miwok.ViewPagerUtilites.CustomFragmentsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_pager);
+        CustomFragmentsPagerAdapter customFragmentsPagerAdapter =
+                new CustomFragmentsPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(customFragmentsPagerAdapter);
 
-    public void changeActivity(View view) {
-        int id = view.getId();
-        Intent intent;
-
-        if (id == R.id.colors) {
-            intent = new Intent(this, ColorsActivity.class);
-        } else if (id == R.id.family) {
-            intent = new Intent(this, FamilyActivity.class);
-        } else if (id == R.id.numbers) {
-            intent = new Intent(this, NumbersActivity.class);
-        } else {
-            intent = new Intent(this, PhrasesActivity.class);
-        }
-
-        startActivity(intent);
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tab_layout);
+//        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/ViewPagerUtilites/CustomFragmentsPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.miwok.ViewPagerUtilites;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.android.miwok.Fragments.ColorsFragment;
+import com.example.android.miwok.Fragments.FamilyFragment;
+import com.example.android.miwok.Fragments.NumbersFragment;
+import com.example.android.miwok.Fragments.PhrasesFragment;
+
+
+public class CustomFragmentsPagerAdapter extends FragmentPagerAdapter {
+    private final Fragment[] fragments = {
+            new ColorsFragment(), new FamilyFragment(), new NumbersFragment(), new PhrasesFragment()
+    };
+    private final CharSequence[] fragmentsTitles = {
+            "Colors", "Family", "Numbers", "Phrases"
+    };
+
+    public CustomFragmentsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments[position];
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentsTitles[position];
+    }
+}
